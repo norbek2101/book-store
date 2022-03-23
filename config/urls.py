de53757b1b5384f09abec8b0ceb78f5903cf70b6
Norbek Kalandarov <eduaction707@gmail.com>
2022-03-23 15:06:04 +0500

    registered 'drf_yas' to installed apps in settings.py in config folder

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="UcharKitob API",
+      default_version='v1',
+      description="UcharKitob API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@ucharkitobapi.uz"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('store.urls')),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
